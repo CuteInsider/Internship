38ce62ee029ced5dbf0cca2ae780db81f520683e
Максим Смирнов <Maksik_999@mail.ru>
2019-03-19 12:11:01 +0300

    Добавьте файлы проекта.

---
+++ MDOUMakeMenu/LoginIn.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace MDOUMakeMenu
+{
+    partial class LoginIn
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginIn));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.linkMenu = new System.Windows.Forms.LinkLabel();
+            this.linkIngredients = new System.Windows.Forms.LinkLabel();
+            this.linkChildren = new System.Windows.Forms.LinkLabel();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel1.Controls.Add(this.linkMenu);
+            this.panel1.Controls.Add(this.linkIngredients);
+            this.panel1.Controls.Add(this.linkChildren);
+            this.panel1.Controls.Add(this.btnBack);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(834, 75);
+            this.panel1.TabIndex = 0;
+            // 
+            // linkMenu
+            // 
+            this.linkMenu.ActiveLinkColor = System.Drawing.Color.White;
+            this.linkMenu.AutoSize = true;
+            this.linkMenu.DisabledLinkColor = System.Drawing.Color.Gray;
+            this.linkMenu.ForeColor = System.Drawing.Color.White;
+            this.linkMenu.LinkBehavior = System.Windows.Forms.LinkBehavior.AlwaysUnderline;
+            this.linkMenu.LinkColor = System.Drawing.Color.White;
+            this.linkMenu.Location = new System.Drawing.Point(12, 26);
+            this.linkMenu.Name = "linkMenu";
+            this.linkMenu.Size = new System.Drawing.Size(65, 23);
+            this.linkMenu.TabIndex = 1;
+            this.linkMenu.TabStop = true;
+            this.linkMenu.Text = "Меню";
+            this.linkMenu.VisitedLinkColor = System.Drawing.Color.White;
+            // 
+            // linkIngredients
+            // 
+            this.linkIngredients.ActiveLinkColor = System.Drawing.Color.White;
+            this.linkIngredients.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.linkIngredients.AutoSize = true;
+            this.linkIngredients.DisabledLinkColor = System.Drawing.Color.Gray;
+            this.linkIngredients.ForeColor = System.Drawing.Color.White;
+            this.linkIngredients.LinkBehavior = System.Windows.Forms.LinkBehavior.AlwaysUnderline;
+            this.linkIngredients.LinkColor = System.Drawing.Color.White;
+            this.linkIngredients.Location = new System.Drawing.Point(264, 26);
+            this.linkIngredients.Name = "linkIngredients";
+            this.linkIngredients.Size = new System.Drawing.Size(74, 23);
+            this.linkIngredients.TabIndex = 1;
+            this.linkIngredients.TabStop = true;
+            this.linkIngredients.Text = "Блюда";
+            this.linkIngredients.VisitedLinkColor = System.Drawing.Color.White;
+            // 
+            // linkChildren
+            // 
+            this.linkChildren.ActiveLinkColor = System.Drawing.Color.White;
+            this.linkChildren.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.linkChildren.AutoSize = true;
+            this.linkChildren.DisabledLinkColor = System.Drawing.Color.Gray;
+            this.linkChildren.ForeColor = System.Drawing.Color.White;
+            this.linkChildren.LinkColor = System.Drawing.Color.White;
+            this.linkChildren.Location = new System.Drawing.Point(522, 26);
+            this.linkChildren.Name = "linkChildren";
+            this.linkChildren.Size = new System.Drawing.Size(56, 23);
+            this.linkChildren.TabIndex = 1;
+            this.linkChildren.TabStop = true;
+            this.linkChildren.Text = "Дети";
+            this.linkChildren.VisitedLinkColor = System.Drawing.Color.White;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBack.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBack.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnBack.Location = new System.Drawing.Point(722, 20);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(100, 35);
+            this.btnBack.TabIndex = 0;
+            this.btnBack.Text = "Выход";
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 75);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(834, 3);
+            this.panel2.TabIndex = 1;
+            // 
+            // LoginIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(834, 462);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(475, 500);
+            this.Name = "LoginIn";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Меню";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.LinkLabel linkMenu;
+        private System.Windows.Forms.LinkLabel linkIngredients;
+        private System.Windows.Forms.LinkLabel linkChildren;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ MDOUMakeMenu/Menu.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace MDOUMakeMenu
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.linkChildren = new System.Windows.Forms.LinkLabel();
+            this.linkMenu = new System.Windows.Forms.LinkLabel();
+            this.linkIngredients = new System.Windows.Forms.LinkLabel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel1.Controls.Add(this.linkMenu);
+            this.panel1.Controls.Add(this.linkIngredients);
+            this.panel1.Controls.Add(this.linkChildren);
+            this.panel1.Controls.Add(this.btnEnter);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(834, 75);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnEnter.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEnter.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnEnter.Location = new System.Drawing.Point(722, 20);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(100, 35);
+            this.btnEnter.TabIndex = 0;
+            this.btnEnter.Text = "Войти";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            // 
+            // linkChildren
+            // 
+            this.linkChildren.ActiveLinkColor = System.Drawing.Color.White;
+            this.linkChildren.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.linkChildren.AutoSize = true;
+            this.linkChildren.DisabledLinkColor = System.Drawing.Color.Gray;
+            this.linkChildren.ForeColor = System.Drawing.Color.White;
+            this.linkChildren.LinkColor = System.Drawing.Color.White;
+            this.linkChildren.Location = new System.Drawing.Point(522, 26);
+            this.linkChildren.Name = "linkChildren";
+            this.linkChildren.Size = new System.Drawing.Size(56, 23);
+            this.linkChildren.TabIndex = 1;
+            this.linkChildren.TabStop = true;
+            this.linkChildren.Text = "Дети";
+            this.linkChildren.VisitedLinkColor = System.Drawing.Color.White;
+            // 
+            // linkMenu
+            // 
+            this.linkMenu.ActiveLinkColor = System.Drawing.Color.White;
+            this.linkMenu.AutoSize = true;
+            this.linkMenu.DisabledLinkColor = System.Drawing.Color.Gray;
+            this.linkMenu.ForeColor = System.Drawing.Color.White;
+            this.linkMenu.LinkBehavior = System.Windows.Forms.LinkBehavior.AlwaysUnderline;
+            this.linkMenu.LinkColor = System.Drawing.Color.White;
+            this.linkMenu.Location = new System.Drawing.Point(12, 26);
+            this.linkMenu.Name = "linkMenu";
+            this.linkMenu.Size = new System.Drawing.Size(65, 23);
+            this.linkMenu.TabIndex = 1;
+            this.linkMenu.TabStop = true;
+            this.linkMenu.Text = "Меню";
+            this.linkMenu.VisitedLinkColor = System.Drawing.Color.White;
+            // 
+            // linkIngredients
+            // 
+            this.linkIngredients.ActiveLinkColor = System.Drawing.Color.White;
+            this.linkIngredients.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.linkIngredients.AutoSize = true;
+            this.linkIngredients.DisabledLinkColor = System.Drawing.Color.Gray;
+            this.linkIngredients.ForeColor = System.Drawing.Color.White;
+            this.linkIngredients.LinkBehavior = System.Windows.Forms.LinkBehavior.AlwaysUnderline;
+            this.linkIngredients.LinkColor = System.Drawing.Color.White;
+            this.linkIngredients.Location = new System.Drawing.Point(264, 26);
+            this.linkIngredients.Name = "linkIngredients";
+            this.linkIngredients.Size = new System.Drawing.Size(74, 23);
+            this.linkIngredients.TabIndex = 1;
+            this.linkIngredients.TabStop = true;
+            this.linkIngredients.Text = "Блюда";
+            this.linkIngredients.VisitedLinkColor = System.Drawing.Color.White;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 75);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(834, 3);
+            this.panel2.TabIndex = 1;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(834, 462);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(475, 500);
+            this.Name = "Menu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Меню";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.LinkLabel linkMenu;
+        private System.Windows.Forms.LinkLabel linkIngredients;
+        private System.Windows.Forms.LinkLabel linkChildren;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
