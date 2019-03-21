8cb8e8a7fb2fe3c9ee9f51c7a779bde55eaa7bf6
Максим Смирнов <Maksik_999@mail.ru>
2019-03-22 00:50:08 +0300

    SQLQueryGenerator V.1

--- MDOUMakeMenu/DataBase.cs
+++ MDOUMakeMenu/DataBase.cs
@@ -4,15 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
-using MySql.Data;
+using System.Data;
 
 namespace MDOUMakeMenu
 {
     class DataBase
     {
-        static MySqlConnection msConnect;
-        public static MySqlCommand msCommand;
-        static MySqlDataAdapter msDataAdapter;
+        protected static MySqlConnection msConnect;
+        protected static MySqlCommand msCommand;
+        protected static MySqlDataAdapter msDataAdapter;
         public static string User = "root";
         public static string PassWord = "qwerty";
         private static string localConnectionString = @"Database = mdou_menu;
@@ -45,8 +45,92 @@ namespace MDOUMakeMenu
         }
     }
 
-    class DataTable : DataBase
+    class Table : DataBase
     {
+        public DataTable newTable(string Query)
+        {
+            DataTable Table = new DataTable();
+            msCommand.CommandText = Query;
+            Table.Clear();
+            msDataAdapter.Fill(Table);
+            return Table;
+        }
 
+        public void Query(string QueryType, params string[][] Fields)
+        {
+            switch (QueryType)
+            {
+                case "SELECT":
+                    msCommand.CommandText = "SELECT ";
+                    for (int i = 0; i < Fields[0].Length; i++)
+                    {
+                        msCommand.CommandText += Fields[0][i];
+                        if (Fields[0][i] != Fields[0].Last())
+                            msCommand.CommandText += ", ";
+                    }
+                    msCommand.CommandText += " FROM ";
+                    for (int i = 0; i < Fields[1].Length; i++)
+                    {
+                        msCommand.CommandText += Fields[1][i];
+                        if (Fields[1][i] != Fields[1].Last())
+                            msCommand.CommandText += ", ";
+                    }
+                    break;
+
+                case "MULTYSELECT":
+                    break;
+
+                case "INSERT":
+                    msCommand.CommandText = "INSERT INTO " + Fields[1][0] + " VALUES(";
+                    for (int i = 0; i < Fields[2].Length; i++)
+                    {
+                        if (Int32.TryParse(Fields[2][i], out int parsed))
+                            msCommand.CommandText += Fields[2][i];
+                        else
+                            msCommand.CommandText += "'"+Fields[2][i]+"'";
+                        if (Fields[2][i] != Fields[2].Last())
+                            msCommand.CommandText += ", ";
+                        else
+                            msCommand.CommandText += ")";
+                    }
+                    break;
+
+                case "UPDATE":
+                    msCommand.CommandText = "UPDATE " + Fields[1][0] + " SET ";
+                    for (int i = 0; i < Fields[0].Length; i++)
+                    {
+                        if (Int32.TryParse(Fields[2][i], out int parsed))
+                            msCommand.CommandText += Fields[0][i] + "= " + parsed;
+                        else
+                            msCommand.CommandText += Fields[0][i] + "= '" + Fields[2][i] + "'";
+                        if (Fields[0][i] != Fields[0].Last())
+                            msCommand.CommandText += ", ";
+                    }
+                    break;
+
+                case "DELETE":
+                    msCommand.CommandText = "DELETE FROM " + Fields[0][0] + " ";
+                    break;
+            }
+            if (Fields[4] != null && QueryType != "INSERT")
+            {
+                msCommand.CommandText += " WHERE  ";
+                for (int i = 0; i < Fields[4].Length; i++)
+                {
+                    msCommand.CommandText += Fields[4][i];
+                    if (Fields[4][i] != Fields[4].Last())
+                        msCommand.CommandText += " AND ";
+                }
+            }
+            msCommand.CommandText += ";";
+            System.Windows.Forms.MessageBox.Show(msCommand.CommandText, "Сообщение");
+            //msCommand.ExecuteNonQuery();
+        }
+
+        public object objQuery(string Query)
+        {
+            msCommand.CommandText = Query;
+            return msCommand.ExecuteScalar();
+        }
     }
 }
--- MDOUMakeMenu/LoginIn.cs
+++ MDOUMakeMenu/LoginIn.cs
@@ -12,6 +12,7 @@ namespace MDOUMakeMenu
 {
     public partial class LoginIn : Form
     {
+        Table test = new Table();
 
         public LoginIn(Point location)
         {
@@ -73,7 +74,21 @@ namespace MDOUMakeMenu
         {
             if (!String.IsNullOrWhiteSpace(txtLogin.Text) && !String.IsNullOrWhiteSpace(txtPassword.Text))
             {
-
+                if (DataBase.Connect())
+                {
+                    //string[][] Fields = new string[3][];
+                    //Fields[0] = new string[1] { "Role" };
+                    //Fields[1] = new string[1] { "users" };
+                    //Fields[2] = new string[2] { "Login =  '"+txtLogin.Text+"'", "Password = '"+txtPassword.Text+"'" };
+                    string[][] Fields = new string[5][];
+                    Fields[0] = new string[2] { "Role", "Digit" };
+                    Fields[1] = new string[1] { "users" };
+                    Fields[2] = new string[2] { "Value" , "233"};
+                    Fields[4] = new string[2] { "Login =  '" + txtLogin.Text + "'", "Password = '" + txtPassword.Text + "'" };
+                    test.Query("INSERT", Fields);
+                }
+                else
+                    MessageBox.Show("Проверте поодключение к базе данных","Ошибка");
             }
         }
     }
